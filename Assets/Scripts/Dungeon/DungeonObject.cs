a33c1e5e772cc66d764a5084792b63417403e122
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2019-09-18 11:01:44 +0200

    Add many room-based classes
     is a base class for all objects that reside in rooms.
    
     is every object that reside in a room tile.
    
     are entities residing in rooms.
    
    In reference of issue #4.

---
+++ Assets/Scripts/Dungeon/DungeonObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public abstract class DungeonObject : MonoBehaviour
+{
+    public Room room;
+
+    public abstract void Load();
+    public abstract void UnLoad();
+}
---
+++ Assets/Scripts/Dungeon/Room.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Room : MonoBehaviour
+{
+    static int gridHeight = 7;
+    static int gridWidth = 13;
+
+    public float width, height;
+    public int x, y;
+    /* Four bits representing whether or not a door is allowed in that direction. */
+    public int doorMask = 0;
+    public RoomTypes types;
+    public Door[] doors;
+
+    public RoomContents contents;
+
+
+    void Awake () {
+        if (contents.room == null) {
+            Debug.Log("bruh");
+            contents = new RoomContents(this, gridWidth, gridHeight);
+        } else {
+            Debug.Log(contents);
+        }
+    }
+}
+
+public enum RoomTypes
+{
+    START, COMMON, TREASURE, BOSS
+}
+
+static class RoomTypesExtentionMethods
+{
+    public static bool IsSpecial(this RoomTypes type)
+    {
+        return type == RoomTypes.TREASURE ||
+               type == RoomTypes.BOSS;
+    }
+}
