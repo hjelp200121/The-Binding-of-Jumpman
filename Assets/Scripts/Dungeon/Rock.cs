ac9be08047db31f2acb63eeaf62ea3d200c973bf
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2019-09-18 11:01:44 +0200

    Add many room-based classes
     is a base class for all objects that reside in rooms.
    
     is every object that reside in a room tile.
    
     are entities residing in rooms.
    
    In reference of issue #4.

---
+++ Assets/Scripts/Dungeon/Rock.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : DungeonTile
+{
+    public override void Load () {
+        gameObject.SetActive(true);
+    }
+    public override void UnLoad () {
+        gameObject.SetActive(false);
+    }
+
+}
---
+++ Assets/Scripts/Dungeon/RoomContents.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class RoomContents
+{
+    public int width, height;
+    public Room room;
+
+    public DungeonTile[] tiles;
+
+    public List<DungeonEntity> entities;
+
+
+    public RoomContents(Room room, int width, int height)
+    {
+        this.room = room;
+        this.width = width;
+        this.height = height;
+        this.tiles = new DungeonTile[width * height];
+        this.entities = new List<DungeonEntity>();
+    }
+
+
+    public DungeonTile GetTile(int x, int y)
+    {
+        return tiles[x + y * width];
+    }
+
+    public void SetTile(DungeonTile tile, int x, int y)
+    {
+        tiles[x + y * width] = tile;
+    }
+}
