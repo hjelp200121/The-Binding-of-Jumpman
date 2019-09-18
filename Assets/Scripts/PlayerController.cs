8ec120ece370d359a80f4e10a2d65d873f9a68d0
hjelp200121 <johan.augsburg@gmail.com>
2019-09-18 18:43:24 +0200

    Finish most of the player #17
    literally used like 6 hours on this shit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,21 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
-    public float speed;
+    //initializations for different stuff
     Rigidbody2D rb;
     public Text keysText;
     public static int keys = 0;
+    public Projectile projectilePrefab;
+    private float lastFire;
+
+    //actual ingame stats
+    //public float health;
+    //public float damage;
+    public float fireDelay;
+    public float shotTimer;
+    public float shotSpeed;
+    public float speed;
+    //public float knockback;
 
     // Start is called before the first frame update
     void Start(){
@@ -18,15 +28,44 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     void Update(){
+        Shoot();
         Move();
         Ui();
     }
 
     public void Move() {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        float vertical = Input.GetAxis("Vertical") * speed;
 
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        if(Input.GetKey("a") == true && Input.GetKey("d") == true) {
+            horizontal = 0;
+        }
+        if (Input.GetKey("w") == true && Input.GetKey("s") == true)
+        {
+            vertical = 0;
+        }
+        rb.velocity = new Vector2(horizontal, vertical);
+    }
+
+    public void Shoot() {
+        float shootHorizontal = 0;
+        float shootVertical = 0;
+        if (Input.GetKey("left")) {
+            shootHorizontal = -1;
+        } else if(Input.GetKey("right")) {
+            shootHorizontal = 1;
+        } else if(Input.GetKey("up")) {
+            shootVertical = 1;
+        } else if(Input.GetKey("down")) {
+            shootVertical = -1;
+        }
+
+        if((shootHorizontal != 0 || shootVertical != 0) && Time.time > lastFire + fireDelay){
+            lastFire = Time.time;
+            Projectile projectile = Instantiate<Projectile>(projectilePrefab, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(shootHorizontal * shotSpeed, shootVertical * shotSpeed);
+            projectile.timer = shotTimer;
+        }
     }
 
     public void Ui() {
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,21 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Key : MonoBehaviour
+public class Projectile : MonoBehaviour
 {
+    public float timer;
+
     // Start is called before the first frame update
     void Start(){
-        
     }
 
     // Update is called once per frame
     void Update(){
-        
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(collision.tag == "Player") {
-            PlayerController.keys++;
+        timer -= Time.deltaTime;
+        if(timer <= 0) {
             Destroy(gameObject);
         }
     }
