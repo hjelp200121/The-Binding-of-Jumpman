a301029e876d7106649b788a980a7b4ce7f1178d
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2019-09-19 16:35:08 +0200

    Set up basic gameplay
    After dungeon generation, the player is spawned in the start room and
    can move from room to room.
    
    In reference of issue #2.
    
    took about 30 minutes.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, -10);
+    }
+}
--- Assets/Scripts/Dungeon/Door.cs
+++ Assets/Scripts/Dungeon/Door.cs
@@ -5,4 +5,16 @@ public class Door : MonoBehaviour
     public Room room;
     public Door connection;
     public Directions direction;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            PlayerController player = other.GetComponent<PlayerController>();
+            /* Move the player to the entrance of the other door. */
+            player.transform.Translate(direction.ToVector() * 3f);
+            connection.room.Load(player);
+            Camera.main.GetComponent<CameraController>().target = connection.room.transform;
+            room.UnLoad();
+        }
+    }
 }
