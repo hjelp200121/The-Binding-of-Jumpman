ae0f0aa7e9e9bca25ae377122a9a054ef5f489b0
hjelp200121 <johan.augsburg@gmail.com>
2019-09-19 15:13:20 +0200

    do something with the player #17
    i think

---
+++ Assets/Scripts/Baba.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BabaState {
+    Wander,
+    Follow,
+    Die,
+};
+
+public class Baba : MonoBehaviour
+{
+    GameObject player;
+    public BabaState currentState = BabaState.Wander;
+
+    public float range;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState) {
+            case (BabaState.Wander):
+                Wander();
+                break;
+
+            case (BabaState.Follow):
+                Follow();
+                break;
+
+            case (BabaState.Die):
+                //Die();
+                break;
+
+        }
+    }
+
+    private bool IsPlayerInRange(float range) {
+        return Vector2.Distance(transform.position, player.transform.position) <= range;
+    }
+
+    void Wander() {
+
+    }
+
+    void Follow() {
+
+    }
+
+}
