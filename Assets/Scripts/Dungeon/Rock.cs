c1c20e660ab93b1c201d291c39b0488cf9bd4fa8
Cornelius Sevald-Krause <cornelius.sevald@gmail.com>
2019-09-20 13:31:28 +0200

    Improve rock
    Now uses the sprites made by @benshapiro69.
    
    In reference of issue #12 and #13.
    
    Took 5 minutes.

--- Assets/Scripts/Dungeon/Rock.cs
+++ Assets/Scripts/Dungeon/Rock.cs
@@ -2,12 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Rock : DungeonTile
 {
-    public override void Load () {
+    public Sprite[] sprites;
+    SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Sprite randSprite = sprites[Random.Range(0, sprites.Length)];
+        spriteRenderer.sprite = randSprite;
+    }
+    public override void Load()
+    {
         gameObject.SetActive(true);
     }
-    public override void UnLoad () {
+    public override void UnLoad()
+    {
         gameObject.SetActive(false);
     }
 
