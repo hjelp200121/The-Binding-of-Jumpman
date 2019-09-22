5f3bbc3ed4dcdd38005229deae6f0dfa1fcda210
hjelp200121 <johan.augsburg@gmail.com>
2019-09-22 22:21:45 +0200

    Adding last items
    Yep #28

---
+++ Assets/Scripts/Items/Complete items/BensGrill.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BensGrill : PassiveItem
+{
+    public Projectile projectilePrefab1;
+    public Projectile projectilePrefab2;
+
+    public override void OnPickup(ItemPedestal pedestal, PlayerController player)
+    {
+        base.OnPickup(pedestal, player);
+        player.hasBensGrill = true;
+        player.Coalprefab = projectilePrefab1;
+        player.BBQprefab = projectilePrefab2;
+    }
+}
---
+++ Assets/Scripts/Items/Complete items/FireFlower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlower : PassiveItem
+{
+    public Projectile projectilePrefab;
+
+    public override void OnPickup(ItemPedestal pedestal, PlayerController player)
+    {
+        base.OnPickup(pedestal, player);
+        player.damage *= 1.3f;
+        player.fireDelay *= 0.8f;
+        player.hasFireFlower = true;
+        player.Fireprefab = projectilePrefab;
+    }
+}
